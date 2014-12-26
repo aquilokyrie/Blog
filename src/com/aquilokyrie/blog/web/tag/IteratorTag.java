0077f751dac9ee04a15abba0f1e700bf09c9b072
aquilokyrie <aquilokyrie@outlook.com>
2014-12-27 00:16:22 +0800

    项目整理

--- src/com/aquilokyrie/blog/web/tag/IteratorTag.java
+++ src/com/aquilokyrie/blog/web/tag/IteratorTag.java
@@ -3,7 +3,6 @@ package com.aquilokyrie.blog.web.tag;
 import java.io.IOException;
 import java.util.Collection;
 import java.util.Hashtable;
-import java.util.Iterator;
 import java.util.Map;
 
 import javax.servlet.jsp.JspException;
@@ -20,12 +19,6 @@ import com.aquilokyrie.blog.util.StringUtil;
  */
 public class IteratorTag extends SimpleTagSupport {
 
-	/**
-	 * 
-	 */
-//	private static final long serialVersionUID = 1076508465193406441L;
-//	private Logger logger = Logger.getInstance(IteratorTag.class);
-	
 	private static final Map<String,Integer> scopeMap = new Hashtable<String,Integer>(4);
 	static{
 		scopeMap.put("page", PageContext.PAGE_SCOPE);
@@ -37,8 +30,6 @@ public class IteratorTag extends SimpleTagSupport {
 	private String collection;//待遍历的集合名
 	private String itemId;//遍历时的名称
 	private String scope;//拉取集合时的查找范围
-	
-//	private Iterator<?> it;
 	
 	/**
 	 * 获取待遍历对象并初始化迭代器
@@ -55,10 +46,7 @@ public class IteratorTag extends SimpleTagSupport {
 				
 				super.getJspBody().invoke(null);
 			}
-		
-		} else {
-//			return BodyTagSupport.SKIP_BODY;  
-		}
+		} 
 	}
 
 	public String getCollection() {
@@ -85,27 +73,4 @@ public class IteratorTag extends SimpleTagSupport {
 		this.scope = scope;
 	}
 	
-//	@Override  
-//    public int doAfterBody() throws JspException {  
-//        if(this.it.hasNext()) {  
-//            //将属性保存在page属性范围之中  
-//            super.pageContext.setAttribute(this.id, this.it.next());  
-//            return BodyTagSupport.EVAL_BODY_AGAIN;  //反复执行doAfterBody()反复  
-//        } else {  
-//            return BodyTagSupport.SKIP_BODY;  
-//        }  
-//    }  
-//  
-//    @Override  
-//    public int doEndTag() throws JspException { //如果此方法没有编写则没有输出  
-//        if(super.bodyContent != null) {  
-//            try {  
-//                super.bodyContent.writeOut(super.getPreviousOut());  
-//            } catch (Exception e) {  
-//  
-//            }  
-//        }  
-//        return BodyTagSupport.EVAL_PAGE;    //正常执行完毕  
-//    }  
-
 }
